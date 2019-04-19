6b27add1fea30fd221ad4801d40240ca06929a42
SushantBansode <sushant.bansode185@gmail.com>
2019-04-19 20:57:08 +0530

    recycler view added

--- app/src/main/java/com/example/upcomingmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/upcomingmoviesapp/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.upcomingmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.upcomingmoviesapp.Util.APIClient;
 import com.example.upcomingmoviesapp.Util.APIInterface;
+import com.example.upcomingmoviesapp.adapter.MovieListAdapter;
 import com.example.upcomingmoviesapp.pojo.GetUpComingMovieResponse;
 
 import retrofit2.Call;
@@ -14,12 +18,14 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity
 {
     APIInterface apiInterface;
+    RecyclerView recycler_movie_list;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recycler_movie_list=findViewById(R.id.recycler_movie_list);
         apiInterface = APIClient.getClient().create(APIInterface.class);
 
         Call<GetUpComingMovieResponse> call = apiInterface.getUpComingMoviesList();
@@ -27,7 +33,13 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onResponse(Call<GetUpComingMovieResponse> call, Response<GetUpComingMovieResponse> response) {
 
-                GetUpComingMovieResponse r=response.body();
+
+
+                MovieListAdapter movieListAdapter = new MovieListAdapter(response.body().getResults(),MainActivity.this);
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(MainActivity.this);
+                recycler_movie_list.setLayoutManager(mLayoutManager);
+                recycler_movie_list.setItemAnimator(new DefaultItemAnimator());
+                recycler_movie_list.setAdapter(movieListAdapter);
 
 
 
--- app/src/main/java/com/example/upcomingmoviesapp/adapter/MovieListAdapter.java
+++ app/src/main/java/com/example/upcomingmoviesapp/adapter/MovieListAdapter.java
@@ -1,31 +1,48 @@
 package com.example.upcomingmoviesapp.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.upcomingmoviesapp.R;
+import com.example.upcomingmoviesapp.pojo.GetUpComingMovieResponse;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MyViewHolder>
 {
-    private String[] mDataset;
+    private GetUpComingMovieResponse.Results[] mMovieList;
+    private Context mContext;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView textView;
-        public MyViewHolder(TextView v) {
+        public TextView item_movie_title,item_movie_release_date,item_movie_adult;
+        public ImageView item_movie_poster;
+
+        public MyViewHolder(View v) {
             super(v);
-            textView = v;
+
+            item_movie_title = (TextView) v.findViewById(R.id.item_movie_title);
+            item_movie_release_date = (TextView) v.findViewById(R.id.item_movie_release_date);
+            item_movie_adult = (TextView) v.findViewById(R.id.item_movie_adult);
+            item_movie_poster=(ImageView)v.findViewById(R.id.item_movie_poster);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MovieListAdapter(String[] myDataset) {
-        mDataset = myDataset;
+    public MovieListAdapter(GetUpComingMovieResponse.Results[] myDataset,Context context) {
+        mMovieList = myDataset;
+        mContext=context;
     }
 
     // Create new views (invoked by the layout manager)
@@ -33,8 +50,9 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MyVi
     public MovieListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        View v =  LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_movie, parent, false);
+
 
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
@@ -45,13 +63,29 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MyVi
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
+       holder.item_movie_title.setText(mMovieList[position].getTitle());
+        holder.item_movie_release_date.setText(mMovieList[position].getRelease_date());
+        holder.item_movie_adult.setText(mMovieList[position].getAdult());
+        String str=mMovieList[position].getPoster_path();
+        String s1="";
+       for (int i=1;i<str.length();i++)
+       {
+           s1=s1+str.charAt(i);
+       }
+       /* Glide.with(mContext).
+                load(s1)
+                .into(holder.item_movie_poster);
+*/
+
+        Picasso.with(mContext).
+                load(s1)
+                .into(holder.item_movie_poster);
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mMovieList.length;
     }
 }
